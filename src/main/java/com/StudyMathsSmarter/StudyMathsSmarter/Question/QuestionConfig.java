fd9047d239e82afaba34a42db1b1c75a4609e184
Jolan1 <jnrewaju1@gmail.com>
2021-10-07 11:23:51 +0100

    questions Config - question names templates

--- src/main/java/com/StudyMathsSmarter/StudyMathsSmarter/Question/QuestionConfig.java
+++ src/main/java/com/StudyMathsSmarter/StudyMathsSmarter/Question/QuestionConfig.java
@@ -12,8 +12,41 @@ public class QuestionConfig {
     @Bean
     CommandLineRunner commandLineRunner(QuestionRepositoryPostgres questionRepositoryPostgres){
         return args -> {
-            // add your questions! :D
-            Question sample = new Question(
+            // add your questions
+            //SOLVING EQUATIONS
+            // QUESTION NAME = 'question' +  topic initial + level + question number
+            Question questionS11 = new Question(
+                    Topics.SOLVING_EQUATION,
+                    "url",
+                    1,
+                    "answer",
+                    "resource link"
+            );
+            questionRepositoryPostgres.saveAll(
+                    List.of(questionS11));
+
+            Question questionS21 = new Question(
+                    Topics.SOLVING_EQUATION,
+                    "url",
+                    2,
+                    "answer",
+                    "resource link"
+            );
+            questionRepositoryPostgres.saveAll(
+                    List.of(questionS21));
+
+            Question questionS31 = new Question(
+                    Topics.SOLVING_EQUATION,
+                    "url",
+                    3,
+                    "answers",
+                    "resource link"
+            );
+            questionRepositoryPostgres.saveAll(
+                    List.of(questionS31));
+
+
+            Question questionG11 = new Question(
                     Topics.SOLVING_EQUATION,
                     "url",
                     1,
@@ -21,8 +54,57 @@ public class QuestionConfig {
                     "resource link"
             );
             questionRepositoryPostgres.saveAll(
-                    List.of(sample)
+                    List.of(questionG11));
+
+            Question questionG21 = new Question(
+                    Topics.SOLVING_EQUATION,
+                    "url",
+                    2,
+                    "answers",
+                    "resource link"
             );
+            questionRepositoryPostgres.saveAll(
+                    List.of(questionG21));
+
+            Question questionG31 = new Question(
+                    Topics.SOLVING_EQUATION,
+                    "url",
+                    3,
+                    "answers",
+                    "resource link"
+            );
+            questionRepositoryPostgres.saveAll(
+                    List.of(questionG31));
+
+            Question questionT11 = new Question(
+                    Topics.SOLVING_EQUATION,
+                    "url",
+                    1,
+                    "answers",
+                    "resource link"
+            );
+            questionRepositoryPostgres.saveAll(
+                    List.of(questionT11));
+
+            Question questionT21 = new Question(
+                    Topics.SOLVING_EQUATION,
+                    "url",
+                    2,
+                    "answers",
+                    "resource link"
+            );
+            questionRepositoryPostgres.saveAll(
+                    List.of(questionT21));
+
+            Question questionT31 = new Question(
+                    Topics.SOLVING_EQUATION,
+                    "url",
+                    3,
+                    "answers",
+                    "resource link"
+            );
+            questionRepositoryPostgres.saveAll(
+                    List.of(questionT31));
         };
     }
 
